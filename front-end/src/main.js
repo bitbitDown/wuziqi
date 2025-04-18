5de557c9fd83f79bee14687d12ddd7ccfb51fa0e
bitbitDown <1415989230@qq.com>
2025-04-18 16:35:59 +0800

    chore:前端与后端通信端口设置

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,7 +6,8 @@ import Socketio from "@/plugins/socket.io";
 const app= createApp(App)
 app.use(i18n)
 app.use(Socketio, {
-  connection:"http://192.168.1.102:3000",// "/* 这里填写服务端地址，如 http://localhost:3000 */",
+  // connection:"http://192.168.1.102:3000",// "/* 这里填写服务端地址，如 http://localhost:3000 */",
+  connection:"http://192.168.1.23:3000",// "/* 这里填写服务端地址，如 http://localhost:3000 */",
   options: {
     autoConnect: false, //关闭自动连接
     // ...其它选项
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 8888,
     host: '0.0.0.0' // 绑定到所有接口
   },
   plugins: [vue()],
